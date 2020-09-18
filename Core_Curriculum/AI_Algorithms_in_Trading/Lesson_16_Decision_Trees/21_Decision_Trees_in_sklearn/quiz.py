276c51c3626f498bd0574a13d171d261dd81dcae
richy1996 <harrisonrichard97@gmail.com>
2020-09-18 14:06:56 -0400

    sep 18

---
+++ Core_Curriculum/AI_Algorithms_in_Trading/Lesson_16_Decision_Trees/21_Decision_Trees_in_sklearn/quiz.py
@@ -0,0 +1,31 @@
+# Import statements 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+# Read the data.
+data = np.asarray(pd.read_csv('data.csv', header=None))
+# Assign the features to the variable X, and the labels to the variable y. 
+X = data[:,0:2]
+y = data[:,2]
+
+# TODO: Create the decision tree model and assign it to the variable model.
+# You won't need to, but if you'd like, play with hyperparameters such
+# as max_depth and min_samples_leaf and see what they do to the decision
+# boundary.
+model = DecisionTreeClassifier()
+
+model.fit(X, y)
+
+# TODO: Make predictions. Store them in the variable y_pred.
+y_pred = model.predict(X)
+
+# TODO: Calculate the accuracy and assign it to the variable acc.
+acc = accuracy_score(y, y_pred)
+
+# max_depth = 16, min_samples_leaf = 2, min_samples_split=0.05
+
+# print(model.tree_.max_depth,'\n',\
+#       model.tree_.min_samples_leaf,'\n',\
+#       model.tree_.min_samples_split)
